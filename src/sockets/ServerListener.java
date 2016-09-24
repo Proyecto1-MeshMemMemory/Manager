12555a37dfcfcf9f096b752171046cd844e78de5
ellioth <ellioth@ubuntu>
2016-09-24 04:51:27 -0600

    escucha para aceptar a los aois que se van conectando y va guardando los mensajes de los apis

--- src/sockets/ServerListener.java
+++ src/sockets/ServerListener.java
@@ -15,18 +15,34 @@ import java.net.Socket;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * clase para hacer las escucha de los api que se conectan y guarda las
+ * conexiones de estas.
+ * @author ellioth
+ */
 public class ServerListener implements Runnable, Constantes{
+    
     private int _port;
     private ServerSocket _server;
     private List<ServerApiListener> _listenerList;
     private DataOutputStream _out;
     private BufferedReader _in;
     
+    /**
+     * constructor de la clase, recibe el puerto para instancear el 
+     * servidor.
+     * @param pPort dato tipo entero.
+     */
     public ServerListener(int pPort){
         _port=pPort;
         _listenerList= new ArrayList<>();
     }
     
+    /**
+     * metodo para obtener el tamaño de la lista de la cantidad de Api's
+     * conectadas.
+     * @return retorna un entero de la cantidad.
+     */
     public int getSizeListListener(){
         int sizeTemp;
         synchronized(this){
@@ -35,6 +51,11 @@ public class ServerListener implements Runnable, Constantes{
         return sizeTemp;
     }
     
+    /**
+     * metodo para obtener la lista de las clases guardadas de los 
+     * apis listeners.
+     * @return retorna un dato tipo List<ServerApiListener>.
+     */
     public List<ServerApiListener> getActualListListeners(){
         List<ServerApiListener> temp;
         synchronized(this){
@@ -43,6 +64,11 @@ public class ServerListener implements Runnable, Constantes{
         return temp;
     }
     
+    /**
+     * metodo para obtener la clase que contiene los datos de la nueva 
+     * conexion del nuevo cliente api.
+     * @return retorna un dato tipo ServerApiListener.
+     */
     public ServerApiListener getNewClientApi(){
         ServerApiListener temp;
         synchronized(this){
@@ -50,17 +76,22 @@ public class ServerListener implements Runnable, Constantes{
         }
         return temp;
     }
-    //public 
+    
+    /**
+     * hilo que se mantendra escuchando los api's que se van conectando.
+     */
     @Override
     public void run() {
         try {
             _server= new ServerSocket(_port);
             System.out.println("Server creado en puerto "+String.valueOf(_port)+
                     ", en "+InetAddress.getLocalHost().getHostAddress());
-            Socket clientSocket=_server.accept();
-            System.out.println("Cliente aceptado");
-            synchronized(this){
-                _listenerList.add(new ServerApiListener(clientSocket));
+            while(true){
+                Socket clientSocket=_server.accept();
+                System.out.println("Cliente aceptado");
+                synchronized(this){
+                    _listenerList.add(new ServerApiListener(clientSocket));
+                }
             }
         } catch (IOException ex) {
             System.out.println("Error: Falla en proceso de creacion de server "
