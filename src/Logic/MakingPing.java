c35ab387ae4a3ba184194de7a3bcaf311c0a97ce
ellioth <ellioth@ubuntu>
2016-09-24 04:56:17 -0600

    cambios agregados al proyecto

--- src/Logic/MakingPing.java
+++ src/Logic/MakingPing.java
@@ -3,12 +3,12 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package Manager;
+package Logic;
 
 /**
  *
  * @author ellioth
  */
-public class GarbageCollet {
+public class MakingPing {
     
 }
