9f6f36dcd9738ed87cd306cf74b65650d8fd196b
ellioth <ellioth@ubuntu>
2016-09-27 00:39:21 -0600

    sistema de pings agregados

--- src/Logic/MakingPing.java
+++ src/Logic/MakingPing.java
@@ -5,10 +5,61 @@
  */
 package Logic;
 
+import Lists.CircularLinkedList;
+import Lists.MiniNode;
+import Lists.SuperNodeMemory;
+
 /**
  *
  * @author ellioth
  */
-public class MakingPing {
+public class MakingPing implements Runnable, Constantes{
+    
+    private CircularLinkedList _circularListToCheck;
+    
+    public MakingPing(CircularLinkedList pCircularListToCheck){
+        _circularListToCheck=pCircularListToCheck;
+    }
+
+    /**
+     * hilo para realizar el ping a los nodos de Android conectados y elimina
+     * los que ya se hayan desconectados.
+     */
+    @Override
+    public void run() {
+        SuperNodeMemory temp;
+        MiniNode tempMiniMaster=null;
+        MiniNode tempMiniSlave=null;
+        int size;
+        synchronized(this){
+            size=_circularListToCheck.getSize();
+            temp=_circularListToCheck.getHead();
+            
+        }
+        for(int i=0; i<size; i++){
+            if(temp.getSlave()!=null){
+                synchronized(this){
+                    tempMiniMaster=temp.getMaster();
+                    tempMiniSlave=temp.getSlave();
+                }
+            }
+            if(tempMiniMaster!=null){
+                if(tempMiniMaster.ping()==DESCONNECTED){
+                    cout("Error: La coneccion con el Master se ha perdido.");
+                    _circularListToCheck.eliminarMiniNode(tempMiniMaster.getID());
+                }
+            }
+            if(tempMiniSlave!=null){
+                if(tempMiniSlave.ping()==DESCONNECTED){
+                    cout("Error: La coneccion con el Master se ha perdido.");
+                    _circularListToCheck.eliminarMiniNode(tempMiniSlave.getID());
+                }
+            }
+            temp=temp.getNext();
+        }
+        
+    }
+    
+    
     
 }
